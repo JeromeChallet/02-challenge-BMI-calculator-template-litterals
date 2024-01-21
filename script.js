0a8b671a235161fd71f07c2d67562e97d7b36bd2
jrme421 <jeromepchallet@gmail.com>
2024-01-21 18:16:20 +0900

    solution

---
+++ script.js
@@ -0,0 +1,11 @@
+let massMark = 56;
+let heightMark = 1.7;
+let massJohn = 89;
+let heightJohn = 1.5;
+
+//BMI = mass / (height * height);
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIJohn, BMIMark);
